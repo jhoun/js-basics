730f264a871f163d0490e7bbeed2289a8c7b6939
Jayh <johnathanhoun@Johnathans-MacBook-Pro.local>
2016-12-05 17:01:04 -1000

    4th test passes: create a function with one parameter

--- basics.js
+++ basics.js
@@ -23,6 +23,10 @@ var person = {
  * and it should print "Hello, my name is {name}"
  */
 
+ var greet = function(name){
+  console.log("Hello, my name is " + name);
+ }
+
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
